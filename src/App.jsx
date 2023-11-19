51a6d4b6a4bcc5e7ca7eb0061c5180537c89d4f8
2709patri <128839994+2709patri@users.noreply.github.com>
2023-11-19 12:55:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistroForm from './RegistroForm';
+
+const App = () => {
+  return (
+    <div>
+      <RegistroForm />
+    </div>
+  );
+};
+
+export default App;
